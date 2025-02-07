44ae028534df4c2348a06d802b3faacaae7847f6
BriannOliveira <brianngomess@gmail.com>
2025-02-07 09:23:02 -0300

    Init test

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fastapi_zero.app import app
+
+client = TestClient(app)
+
+def test_hello():
+    response = client.get('/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'Hello': 'World'}
