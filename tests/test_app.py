61591b7677a43e5962a9d302b6f08a2cc84da4cd
BriannOliveira <brianngomess@gmail.com>
2025-02-10 15:36:10 -0300

    Add CRUD routes

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,4 +10,58 @@ client = TestClient(app)
 def test_hello():
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'Hello': 'World'}
+    assert response.json() == {'message': 'Hello World'}
+
+
+def test_create_user():
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'John Doe',
+            'email': 'johndoe@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'John Doe',
+        'email': 'johndoe@example.com',
+        'id': 1,
+    }
+
+
+def test_list_users():
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'John Doe',
+                'email': 'johndoe@example.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user():
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'Jane Doe',
+            'email': 'johndoe1@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'Jane Doe',
+        'email': 'johndoe1@example.com',
+        'id': 1,
+    }
+
+
+def test_delete_user():
+    response = client.delete('/users/1')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
